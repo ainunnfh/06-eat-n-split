011f0ce59d7de7edc4059f5fd00be9fabea52c70
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-02-29 11:29:54 +0700

    ⭐New (FormAddFriend): create new component

---
+++ src/FormAddFriend.js
@@ -0,0 +1,13 @@
+export default function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>Friend Name</label>
+      <input type="text"></input>
+
+      <label> Image URL</label>
+      <input type="text"></input>
+
+      <button className="button">Add</button>
+    </form>
+  );
+}
