ac20f109c0d21cd1f7013da54cf6936178b70675
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-03-01 09:34:02 +0700

    🆙Update (App): state for button add friend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import Button from "./Button";
 import FormAddFriend from "./FormAddFriend";
 import FormSplitBill from "./FormSplitBill";
 import FriendLists from "./FriendLists";
+import { useState } from "react";
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendLists />
-        <FormAddFriend />
-        <button className="button">Add Friend</button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
