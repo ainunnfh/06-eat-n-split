92d514b2e64d05b89b95f898142faf501811f373
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-03-01 09:09:26 +0700

    💄Update (FormAddFriend): add icon

--- src/FormAddFriend.js
+++ src/FormAddFriend.js
@@ -1,10 +1,10 @@
 export default function FormAddFriend() {
   return (
     <form className="form-add-friend">
-      <label>Friend Name</label>
+      <label>👯 Friend Name</label>
       <input type="text"></input>
 
-      <label> Image URL</label>
+      <label>🖼️ Image URL</label>
       <input type="text"></input>
 
       <button className="button">Add</button>
