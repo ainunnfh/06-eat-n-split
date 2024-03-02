833b4b1bc0d35e0bbca1fdda7dbe0e3166c742a4
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-03-02 10:05:54 +0700

    🆙Update (FriendList): Lift up friendList state

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ import FormAddFriend from "./FormAddFriend";
 import FormSplitBill from "./FormSplitBill";
 import FriendLists from "./FriendLists";
 import { useState } from "react";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
 export default function App() {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
   }
+ 
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendLists />
-        {showAddFriend && <FormAddFriend />}
+        <FriendLists friends={friends} />
+        {showAddFriend && <FormAddFriend  />}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Close" : "Add Friend"}
         </Button>
