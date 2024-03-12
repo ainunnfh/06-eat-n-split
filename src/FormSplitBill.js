f2f40aa63fb8a0da85f9d918a5b0f4964f5b8113
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-03-12 11:21:53 +0700

    🆙Update (FormSplitBill): add props for get name

--- src/FormSplitBill.js
+++ src/FormSplitBill.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Button from "./Button";
 
-const FormSplitBill = () => {
+const FormSplitBill = ({ selectedFriend }) => {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
+      <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰Your expense</label>
       <input type="text"></input>
-      <label>🧑‍🤝‍🧑X's expense</label>
+      <label>🧑‍🤝‍🧑{selectedFriend.name}'s expense</label>
       <input type="text" disabled></input>
 
       <label>💡Who is paying the bill?</label>
       <select>
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
       <Button>Split</Button>
     </form>
