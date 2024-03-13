4025f223fc6770697d3b2d7b71e711447d95444f
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-03-13 15:32:39 +0700

    🆙Update (FormSplitBill): passing split bill handler as a props

--- src/FormSplitBill.js
+++ src/FormSplitBill.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import Button from "./Button";
 
-const FormSplitBill = ({ selectedFriend }) => {
+const FormSplitBill = ({ selectedFriend, onSplitBill }) => {
   const [bill, setBill] = useState("");
   const [paidByUser, setPaidByUser] = useState("");
   const [whoIsPaying, setWhoIsPaying] = useState("user");
 
   const paidByFriend = bill ? bill - paidByUser : "";
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) return;
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+  }
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💵Bill value</label>
